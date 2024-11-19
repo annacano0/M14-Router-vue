0e55e67ee953603c34d2b9e8f87331f362da42c7
Anna <anna.cano.7e7@itb.cat>
2024-11-19 19:43:42 +0100

    add: productDetail

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import products from '../assets/productos.json'
+</script>
 
 <template>
-  <h1>Product detail:</h1>
+  <h1>Product detail</h1>
+  <p>{{ $route.params.id }}</p>
+  <p>{{ products[$route.params.id - 1].name }}</p>
+  <p>{{ products[$route.params.id - 1].description }}</p>
 </template>
 
 <style></style>
