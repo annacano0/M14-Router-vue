f2b1276ca2b4967666740b58160cf01a89d893a4
Anna <anna.cano.7e7@itb.cat>
2024-11-19 19:23:14 +0100

    first commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import productList from '@/assets/productos.json'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <p>Esto es una lista de productos:</p>
+  <ul>
+    <li v-for="product in productList" :key="product.id">
+      <RouterLink :to="'/products/' + product.id">{{ product.name }}</RouterLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ProductList from '@/components/ProductList.vue'
+</script>
+
+<scrip setup>
+
+</scrip>
+<template>
+  <h1>Product page</h1>
+  <ProductList />
+</template>
+
+<style></style>
